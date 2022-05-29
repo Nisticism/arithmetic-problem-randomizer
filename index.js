6e23fb6ad10421477e743cb3bec9286831e9042f
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2022-05-28 22:22:23 -0700

    mathjs linked

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let numberA = 0;
+let numberB = 0;
+
 function test() {
   console.log("test");
+  console.log(randomize(3, 10));
+}
+
+function randomize(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
 }
