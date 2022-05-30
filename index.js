9356062d54367af150b3427bc8e677e39748596a
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2022-05-29 18:54:27 -0700

    basic addition functionality added

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 let numberA = 0;
 let numberB = 0;
+let streak = 0;
+let minValue = 1;
+let maxValue = 10;
+const title = document.getElementById("app-title");
+const problem = document.getElementById("problem");
+const input = document.getElementById("input");
+const streakText = document.getElementById("streak");
+const invis = document.getElementById("invis");
 
-function test() {
-  console.log("test");
-  console.log(randomize(3, 10));
+function setup() {
+  title.innerText = "Arithmetic Problem Randomizer"
+  problem.innerText = randomizeProblem(minValue, maxValue);
+}
+
+function randomizeProblem(min, max) {
+  numberA = randomize(min, max);
+  numberB = randomize(min, max);
+  return "What is " + numberA + " + " + numberB + "?";
+}
+
+function answer() {
+  let answer = parseInt(input.value);
+  if (answer && (numberA + numberB) == answer) {
+    streak += 1;
+    alert("correct");
+    maxValue += 1;
+    problem.innerText = randomizeProblem(minValue, maxValue);
+  } else {
+    streak = 0;
+    alert("incorrect");
+    maxValue = 10;
+  }
+  streakText.innerText = "Streak: " + streak;
+  console.log(input.value);
 }
 
 function randomize(min, max) {
